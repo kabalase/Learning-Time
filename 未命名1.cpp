d2ba95fada2951eed1d780ac413a4c2b01664bdd
kabalase <zzh2897978039@Gmail.com>
2025-10-18 00:05:21 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+int main()
+{
+int price=0;
+int bill=0;
+printf(" ‰»ÎΩ∂Ó");
+scanf("%d",&price);
+printf(" ‰»Î∆±√Ê");
+scanf("%d",&bill);
+if(bill>=price){
+printf("’“¡„%d",bill-price);
+}else{
+printf("«Æ≤ªπª£°«ÓπÌ\n");
+}
+
+return 0;
+}
+
