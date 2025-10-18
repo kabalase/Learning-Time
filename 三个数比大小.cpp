af0306149dc5dc53a103e7f384f2ca3bd6f9de7a
kabalase <zzh2897978039@Gmail.com>
2025-10-18 17:33:08 +0800

    Add files via upload

---
+++ 三个数比大小.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("请输入三个数");
+	scanf("%d %d %d",&a,&b,&c);
+	int max=0;
+	if(a>b){
+		if(a>c){
+		max=a;}
+		else{max=c;
+		}
+	}else{
+		if(b>c){
+		max=b;}
+		else{max=c;
+		}
+	}
+	printf("最大值为%d",max);	
+	
+	
+}
